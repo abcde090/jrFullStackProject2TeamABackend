5cfad56aa8a41bb3aec6f80a61eeba037490f385
hjjhao <hjjhao@gmail.com>
2019-03-13 12:27:52 +1000

    add user

--- src/models/user.js
+++ src/models/user.js
@@ -2,12 +2,28 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema(
 	{
-		_id: {
+		name: {
 			type: String,
-			uppercase: true,
-			alias: 'code'
+			required: true,
+			trim: true,
+			minlength: 2
 		},
-		role: String,
+		email: {
+			type: String,
+			required: true,
+			trim: true,
+			unique: true,
+			// cannot validate the uniqueness here, it will break the update function
+			validate: {
+				validator: email => !Joi.validate(email, Joi.string().email()).error,
+				msg: 'Invalid email format'
+			}
+		},
+		password: {
+			type: String,
+			required: true,
+			select: false
+		},
 	},
 	{
 		timestamps: true,
@@ -20,5 +36,14 @@ const userSchema = new mongoose.Schema(
 		id: false
 	}
 );
+userSchema.methods.hashPassword = async function () {
+	this.password = await bcrypt.hash(this.password, 10);
+};
+
+userSchema.methods.validatePassword = async function (password) {
+	const validPassword = await bcrypt.compare(password, this.password);
+	return validPassword;
+};
+
 
 module.exports = mongoose.model('User', userSchema);
