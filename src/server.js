6112b0cb5d614ac1d7424120b7d04430b82de33a
hjjhao <hjjhao@gmail.com>
2019-05-02 20:08:07 +1000

    add DB setting

--- src/server.js
+++ src/server.js
@@ -10,7 +10,7 @@ const logger = require('./utils/logger');
 const notFoundHandler = require('./middlewares/notFound');
 const errorHandler = require('./middlewares/errorHandler');
 const PORT = process.env.PORT || 3000;
-const swaggerUi=require('swagger-ui-express')
+const swaggerUi = require('swagger-ui-express')
 const swaggerDocument = require('../swagger/swagger.json');
 process.on('uncaughtException', e => {
 	logger.error(e.message);
@@ -24,40 +24,48 @@ process.on('unhandledRejection', e => {
 
 const app = express();
 
- 
+
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use(bodyParser.json());
-app.all('/api/*', function (req, res, next) {
-	console.log('dadad');
-	const allowedOrigins = ['http://localhost:3111','http://localhost:8000', 'https://learn.jiangren.com.au', 'http://learn.jiangren.com.au', 'http://127.0.0.1:8080','http://jrdashboard.s3-website-ap-southeast-2.amazonaws.com'];
-	const origin = req.headers.origin;
-	if (allowedOrigins.indexOf(origin) > -1) {
-		res.setHeader('Access-Control-Allow-Origin', origin);
-	}
-	res.header('Access-Control-Allow-Credentials', true);
-	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE, PATCH');
-	res.header('Access-Control-Allow-Headers', 'Origin, Authorization, x-xsrf-token,X-Requested-With, Content-Type, Accept, x-access-token');
-	//intercepts OPTIONS method
-	if ('OPTIONS' === req.method) {
-		//respond with 200
-		res.send(200);
-	} else {
-		next();
-	}
-});
+// app.all('/api/*', function (req, res, next) {
+// 	console.log('dadad');
+// 	const allowedOrigins = ['http://localhost:3111','http://localhost:8000', 'https://learn.jiangren.com.au', 'http://learn.jiangren.com.au', 'http://127.0.0.1:8080','http://jrdashboard.s3-website-ap-southeast-2.amazonaws.com'];
+// 	const origin = req.headers.origin;
+// 	if (allowedOrigins.indexOf(origin) > -1) {
+// 		res.setHeader('Access-Control-Allow-Origin', origin);
+// 	}
+// 	res.header('Access-Control-Allow-Credentials', true);
+// 	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE, PATCH');
+// 	res.header('Access-Control-Allow-Headers', 'Origin, Authorization, x-xsrf-token,X-Requested-With, Content-Type, Accept, x-access-token');
+// 	//intercepts OPTIONS method
+// 	if ('OPTIONS' === req.method) {
+// 		//respond with 200
+// 		res.send(200);
+// 	} else {
+// 		next();
+// 	}
+// });
 app.use(helmet());
 app.use(cors());
 if (process.env.NODE_ENV === 'development') {
-  	app.use(morgan('dev'));
+	app.use(morgan('dev'));
 } else {
-  	app.use(morgan('common'));
+	app.use(morgan('common'));
 }
 
 app.use(routes);
 app.use(errorHandler);
 app.use(notFoundHandler);
 
-const connectionUrl = 'mongodb://localhost:27017/jr-ipm';
+// 
+const { DB_USER, DB_HOST, DB_PASSWORD, DB_DATABASE } = process.env;
+let connectionUrl;
+if (DB_USER && DB_PASSWORD) {
+	connectionUrl =
+		`mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_DATABASE}?retryWrites=true`
+} else {
+	connectionUrl = 'mongodb://localhost:27017/jr-ipm';
+}
 mongoose
 	.connect(connectionUrl, { useNewUrlParser: true })
 	.then(() => {
